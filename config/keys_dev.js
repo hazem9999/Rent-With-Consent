217868c5c9efa3c450564cea5b518374d6f167b1
hazem9999 <hazem.elhakim@student.guc.edu.eg>
2019-11-24 23:09:55 +0200

    update keydevs

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,4 @@
 module.exports = {
     mongoURI: 'mongodb+srv://hazem9999:gemseldeen@rent-with-consent-eoozu.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'verysecretkey' 
-    
+    secretOrKey: 'verysecretkey'  
 }
